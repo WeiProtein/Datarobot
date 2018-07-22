19452e6ea2535fb61a7911ea769f2a481b3f381d
Wei-Ming Koh <Timothy.Wei-Ming.Koh@ibm.com@Wei-Mings-MacBook-Pro.local>
2018-07-22 12:05:58 -0400

    still trying

--- create_repo.py
+++ create_repo.py
@@ -10,6 +10,19 @@ password = getpass.getpass('Please enter your password: ')
 
 g = Github(userName, password)
 
+#print out all repos in user's github
 for repo in g.get_user().get_repos():
     print(repo.name)
     repo.edit(has_wiki=False)
+
+#using shell to execute git command
+def execute(cmd, work_dir):
+     pipe = subprocess.Popen(cmd, shell=True, cwd=work_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, error) = pipe.communicate()
+    print out, error
+    pipe.wait()
+
+#adds file to index
+def git_add(file_path, repo_dir):
+    cmd = 'git add ' + file_path
+    execute(cmd, repo_dir)
